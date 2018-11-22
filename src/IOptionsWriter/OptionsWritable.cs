422e6e8a8edb25f8d8bff7aefc1eb7cea1c38059
Alexander Salamatov <asalamatov@appulate.com>
2018-11-22 17:47:03 +0300

    create settings file if not exists

--- src/IOptionsWriter/OptionsWritable.cs
+++ src/IOptionsWriter/OptionsWritable.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using JStreamAsyncNet;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -43,18 +44,25 @@ namespace IOptionsWriter
 
 		public async Task Update(Action<T> applyChanges)
 		{
-			var path = _environment.ContentRootFileProvider.GetFileInfo(_settingsFile).PhysicalPath;
+			IFileInfo settingsFileInfo = _environment.ContentRootFileProvider.GetFileInfo(_settingsFile);
+			JObject jObject;
+			if (!settingsFileInfo.Exists)
+			{
+				var n = new T();
+				applyChanges(n);
+				jObject = JObject.FromObject(n);
+			}
+			else
+			{
+				jObject = await File.OpenRead(settingsFileInfo.PhysicalPath).ToObject<JObject>();
+				var sectionObject = jObject.TryGetValue(_section, out JToken section)
+					? JsonConvert.DeserializeObject<T>(section.ToString())
+					: Value ?? new T();
+				applyChanges(sectionObject);
+				jObject[_section] = JObject.Parse(JsonConvert.SerializeObject(sectionObject));
+			}
 
-			var jObject = await File.OpenRead(path).ToObject<JObject>();
-			var sectionObject = jObject.TryGetValue(_section, out JToken section)
-				? JsonConvert.DeserializeObject<T>(section.ToString())
-				: Value ?? new T();
-
-			applyChanges(sectionObject);
-
-			jObject[_section] = JObject.Parse(JsonConvert.SerializeObject(sectionObject));
-
-			await File.Create(path).FromObject(jObject,
+			await File.Create(settingsFileInfo.PhysicalPath).FromObject(jObject,
 				JsonSerializer.Create(new JsonSerializerSettings {Formatting = Formatting.Indented}));
 
 			if (_reloadAfterWrite) _configurationRoot.Reload();
